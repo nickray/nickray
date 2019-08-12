94bc9b90e148c02e85dfba1cdcb3e52e733c0df8
Nicolas Stalder <n@stalder.io>
2019-08-12 12:51:16 +0200

    Initial commit

---
+++ src/2019-08-12-hello-world.rs
@@ -0,0 +1,19 @@
+/** # Hello, world!
+
+<sup>*by [Nicolas Stalder]&#8202;,&ensp;2019-08-12*</sup>
+
+[Nicolas Stalder]: https://github.com/nickray
+
+<br>
+
+Hello, world!
+
+*/
+
+#[macro_export]
+macro_rules! _01__hello_world {
+    ({
+        date:  "August 12, 2019",
+        author:  "Nicolas Stalder",
+    }) => {};
+}
---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+#![allow(non_snake_case)]
+
+#[path = "2019-08-12-hello-world.rs"]
+mod _01;
